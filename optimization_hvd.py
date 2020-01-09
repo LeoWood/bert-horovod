869236ed9e94667b27b62529cdaba79f9e5280c7
LeoWood <463861936@qq.com>
2020-01-09 16:24:07 +0800

    try new lr

--- optimization_hvd.py
+++ optimization_hvd.py
@@ -58,8 +58,8 @@ def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu):
   # is how the model was trained (note that the Adam m/v variables are NOT
   # loaded from init_checkpoint.)
   optimizer = AdamWeightDecayOptimizer(
-      # learning_rate=learning_rate * hvd.size(),
-      learning_rate=learning_rate,
+      learning_rate=learning_rate * hvd.size(),
+      # learning_rate=learning_rate,
       weight_decay_rate=0.01,
       beta_1=0.9,
       beta_2=0.999,
